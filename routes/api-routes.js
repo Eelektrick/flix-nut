104e23229ed7a2341cad934c3b35b7dd8895c92c
diembe <contact@antibean.com>
2020-09-07 16:28:06 -0600

    initial api implementation

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,6 +17,11 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function(req, res) {
+
+	console.log("We got to the signup api route");
+	console.log("email: " + req.body.email);
+	console.log("password: " + req.body.password);
+
     db.User.create({
       email: req.body.email,
       password: req.body.password
@@ -25,6 +30,7 @@ module.exports = function(app) {
         res.redirect(307, "/api/login");
       })
       .catch(function(err) {
+		console.log("This is the error catch");
         res.status(401).json(err);
       });
   });
